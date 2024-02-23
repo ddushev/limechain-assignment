6516550217f53405869cb27050d538e65f0cb8f1
Daniel Dushev <danieldyshew@gmail.com>
2024-02-23 10:36:54 +0200

    beerDetails. add check beer number functionality

--- src/components/beerDetails/beerDetails.tsx
+++ src/components/beerDetails/beerDetails.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { sepolia } from "wagmi/chains"
 import { useReadContract, useWriteContract } from 'wagmi';
 
@@ -7,12 +7,15 @@ import CONTRACT from "../../constants/contract";
 
 import styles from "./beerDetails.module.scss";
 import { BaseError } from "viem";
+import PATHS from "../../constants/paths";
 
 
 export default function BeerDetails() {
+    const navigate = useNavigate();
     const [rating, setRating] = useState("");
+    const [beerNumber, setBeerNumber] = useState("");
     const [message, setMessage] = useState("");
-    const { data: hash, error, writeContract } = useWriteContract()
+    const { data: hash, error, writeContract } = useWriteContract();
     const { beerId } = useParams();
     const { data: numberOfBeers } = useReadContract({
         abi: CONTRACT.ABI,
@@ -29,8 +32,12 @@ export default function BeerDetails() {
         chainId: sepolia.id,
     });
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleRatingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setRating(event.target.value);
+    }
+
+    const handleBeerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setBeerNumber(event.target.value);
     }
 
     const handleSendClick = () => {
@@ -41,10 +48,15 @@ export default function BeerDetails() {
         writeContract({
             abi: CONTRACT.ABI,
             address: `0x${CONTRACT.ADDRESS}`,
-            functionName: 'rateBeer',
+            functionName: "rateBeer",
             args: [BigInt(beerId!), Number(rating)],
         })
         setMessage("");
+    }
+
+    const handleCheckBeer = () => {
+        navigate(`${PATHS.BEER_DETAILS}/${Number(beerNumber) - 1}`);
+        setBeerNumber("");
     }
 
     if (Number(beerId) >= Number(numberOfBeers)) {
@@ -62,9 +74,22 @@ export default function BeerDetails() {
                     <h2 className={styles.detailsHeading}>Loading...</h2>
                     :
                     <>
-                        <h2 className={styles.detailsHeading}>Details for beer number {beerId} of total {Number(numberOfBeers)} beers in the smart contract</h2>
+                        <h2 className={styles.detailsHeading}>Details for beer number {Number(beerId) + 1} of total {Number(numberOfBeers)} beers in the smart contract</h2>
                         {beerDetails &&
                             <>
+                                <div>
+                                    <label htmlFor="beerNumber"><span className={styles.boldText}>Check Beer Number:</span> </label>
+                                    <input className={styles.ratingInput}
+                                        min={0}
+                                        max={Number(numberOfBeers)}
+                                        type="number"
+                                        name="beerNumber"
+                                        id="beerNumber"
+                                        placeholder={`0 to ${Number(numberOfBeers)}`}
+                                        value={beerNumber}
+                                        onChange={handleBeerChange} />
+                                </div>
+                                <button onClick={handleCheckBeer} className={styles.btn} type="button">Check Beer</button>
                                 <p><span className={styles.boldText}>Beer Name:</span> {beerDetails[0]}</p>
                                 <img className={styles.beerImg} src={beerDetails[1]} alt="beer-img" />
                                 <p><span className={styles.boldText}>Brewery:</span> {beerDetails[2]}</p>
@@ -73,10 +98,18 @@ export default function BeerDetails() {
                                 <p><span className={styles.boldText}>Price:</span> ${Number(beerDetails[5])}</p>
                                 <p><span className={styles.boldText}>Average Rating:</span> {Number(beerDetails[6])}</p>
                                 <div>
-                                    <label htmlFor="ratingInput"><span className={styles.boldText}>Your rating:</span> </label>
-                                    <input className={styles.ratingInput} min={1} max={5} type="number" name="ratingInput" id="ratingInput" placeholder="1 to 5" value={rating} onChange={handleInputChange} />
+                                    <label htmlFor="ratingInput"><span className={styles.boldText}>Your Rating:</span> </label>
+                                    <input className={styles.ratingInput}
+                                        min={1}
+                                        max={5}
+                                        type="number"
+                                        name="ratingInput"
+                                        id="ratingInput"
+                                        placeholder="1 to 5"
+                                        value={rating}
+                                        onChange={handleRatingChange} />
                                 </div>
-                                <button onClick={handleSendClick} className={styles.rateBtn} type="button">Send</button>
+                                <button onClick={handleSendClick} className={styles.btn} type="button">Rate</button>
                                 {hash && <p><span className={styles.boldText}>Transaction hash:</span> {hash}</p>}
                                 {message && <p><span className={styles.boldText}>Invalid input:</span> {message}</p>}
                                 {error && (
